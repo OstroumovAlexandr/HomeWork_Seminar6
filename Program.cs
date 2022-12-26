a541e0f994693258d2a349e09ce18bd6ed0e4d66
Alexandr Ostroumov <m0nstrya9a@gmail.com>
2022-12-26 11:46:32 +0300

    add task 2 & task 3

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 1:
+
+// Проверка на палиндромность 5-ти значного числа
+
+
+
+
+
+
+// Задача 2 
+// Нахождене расстояния между двумя точками в 3D простарнстве
+/*
+Console.WriteLine("Введите координату Х точки А: ");
+int xa = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату Y точки A: ");
+int ya = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату Z точки A: ");
+int za = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату x точки b: ");
+int xb = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату Y точки B: ");
+int yb = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату Z точки B: ");
+int zb = int.Parse(Console.ReadLine()!);
+
+double result = Math.Sqrt(Math.Pow(xb-xa, 2) + Math.Pow(yb-ya, 2) + Math.Pow(zb-za, 2));
+
+Console.WriteLine($"Расстояние между точками: {result:f2}"); // {result:f2} после двоеточия стоит конструкция, 
+                                                            // которая определяет количество выводимых знаков после запятой. 
+                                                           // 2 - означает два знака
+
+*/
+
+
+
+
+
+
+// Задача 3
+
+// Таблица кубов чисел от 1 до N
+/*
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+for(int i = 1; i <= n; i++)
+{
+    Console.Write($"{Math.Pow(i,3)} ");
+}
+
+*/
